2d42fff121a81375f012941fc6050601c64eda81
Daniel Frey <dann.frey@me.com>
2018-07-16 14:40:45 -0700

    added more locations

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var franchise = {
-  location: 'pike',
-  actualName: '1st and Pike',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookieSales: 6.3
+var pikeLocation = {
+  displayName: '1st and Pike',
+  minCustomerHour: 23,
+  maxCustomerHour: 65,
+  avgCookies: 6.3
+};
+var seaTacLocation = {
+  displayName: 'SeaTac Airport',
+  minCustomerHour: 3,
+  maxCustomerHour: 24,
+  avgCookies: 1.2
+};
+var centerLocation = {
+  displayName: 'Seattle Center',
+  minCustomerHour: 11,
+  maxCustomerHour: 38,
+  avgCookies: 3.7
+};
+var capitolHillLocation = {
+  displayName: 'Capitol Hill',
+  minCustomerHour: 20,
+  maxCustomerHour: 38,
+  avgCookies: 2.3
+};
+var alkiLocation = {
+  displayName: 'Alki',
+  minCustomerHour: 2,
+  maxCustomerHour: 16,
+  avgCookies: 4.6
 };
